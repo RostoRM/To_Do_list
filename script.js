0347ada0a7c200175a2a3603a101177662cce795
abu-101 <92208542+abu-101@users.noreply.github.com>
2022-02-04 14:42:13 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 //Cashing DOM
-var enterBtn = document.getElementById('enter');
-var ColorBtn = document.getElementById('backColorBtn');
-var deleteBtn_div = document.getElementsByClassName(`deleteBtn`);
-var input = document.getElementById('userinput');
-var ul = document.querySelector('#shopping_list__items');
-var body = document.getElementsByClassName(`gradient`);
+const enterBtn = document.getElementById('enter');
+const ColorBtn = document.getElementById('backColorBtn');
+const deleteBtn_div = document.getElementsByClassName(`deleteBtn`);
+const input = document.getElementById('userinput');
+const ul = document.querySelector('#shopping_list__items');
+const body = document.getElementsByClassName(`gradient`);
 
 //Create function to toggle li elements with .done class/
-function toggleDoneClass(e) {
+const toggleDoneClass = (e) => {
   const trgt = e.target.closest(`li`);
   if (trgt) trgt.classList.toggle(`done`);
-}
+};
 
 //Accessing input element Value /
-function inputLength() {
+const inputLength = () => {
   return input.value.length;
-}
+};
 
 //Create and Append elements,add classes. Execution of main(delete) function inside of createListElement()/
-function createListElement() {
-  var div = document.createElement(`div`);
+const createListElement = () => {
+  let div = document.createElement(`div`);
   ul.appendChild(div);
 
-  var li = document.createElement('li');
+  let li = document.createElement('li');
   li.appendChild(document.createTextNode(input.value));
   div.appendChild(li);
   input.value = ``;
 
-  var delbtn = document.createElement(`button`);
+  let delbtn = document.createElement(`button`);
   delbtn.appendChild(document.createTextNode(`Delete Button`));
   div.appendChild(delbtn);
 
@@ -36,36 +36,36 @@ function createListElement() {
   li.classList.add(`li_listItem`);
 
   main();
-}
+};
 
 //Create above elements if inputLength is > 0  with mouse click
-function addListAfterClick() {
+const addListAfterClick = () => {
   if (inputLength() > 0) {
     createListElement();
   }
-}
+};
 //Create above elements if inputLength is > 0  with enter key
-function addListAfterKeypress(event) {
+const addListAfterKeypress = (event) => {
   if (inputLength() > 0 && event.keyCode === 13) {
     createListElement();
   }
-}
+};
 // random color function
-function randomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
+const randomColor = () => {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 // show Rendom Colors on backgrounds
 const showRendomColor = () => {
-  const rgbvalue1 = randomColor();
-  const rgbvalue2 = randomColor();
+  let rgbvalue1 = randomColor();
+  let rgbvalue2 = randomColor();
   // const rgbvalue3 = randomColor();
 
-  const RgbColorProperty = `linear-gradient(to right,${rgbvalue1},${rgbvalue2})`;
+  let RgbColorProperty = `linear-gradient(to right,${rgbvalue1},${rgbvalue2})`;
   for (const allbody of body) {
     allbody.style.background = RgbColorProperty;
   }
@@ -79,13 +79,13 @@ const showRendomColor = () => {
 
 //Executiing  DeleteFunction,which deletes elements and main Function which executes eventListeners /
 
-function main() {
+const main = () => {
   function deleteShoppingList() {
     this.parentElement.remove();
   }
 
-  for (var i = 0; i < deleteBtn_div.length; i++) {
-    var clickOnDltButton = deleteBtn_div[i];
+  for (let i = 0; i < deleteBtn_div.length; i++) {
+    let clickOnDltButton = deleteBtn_div[i];
     clickOnDltButton.addEventListener('click', deleteShoppingList, false);
   }
 
@@ -96,6 +96,6 @@ function main() {
   ul.addEventListener(`click`, toggleDoneClass);
 
   ColorBtn.addEventListener(`click`, showRendomColor);
-}
+};
 
 main();
